164b4446f304a1f3c8f70188669f78112b7aea82
SHUBHAM <154758475+onceagainarise@users.noreply.github.com>
2025-03-20 19:27:37 +0530

    Update detect_language.py

--- scripts/detect_language.py
+++ scripts/detect_language.py
@@ -2,22 +2,22 @@ import pandas as pd
 import langid
 
 try:
-    data = pd.read_csv('C:/Users/nishi/Documents/project/Code/data/preprocessed_data.csv')
+    data = pd.read_csv('C:/Users/preprocessed_data.csv')
 
-    # Print available columns for debugging
+ 
     print("Available columns:", data.columns)
 
-    # Ensure all values in 'cleaned_text' are strings
+    
     data['cleaned_text'] = data['cleaned_text'].astype(str)
 
-    # Classify the language of the cleaned text
+   
     data['detected_language'] = data['cleaned_text'].apply(lambda x: langid.classify(x)[0])
 
-    # Print to check the DataFrame
+    
     print("Detected languages:")
     print(data[['cleaned_text', 'detected_language']].head())
 
-    # Save the results
+
     data.to_csv('C:/Users/nishi/Documents/project/Code/data/language_detection_data.csv', index=False)
 
 except Exception as e:
